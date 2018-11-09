44ad7679f496c5e35e4be4c4000c3df5e274739c
TarunApp <tappannagari@gmail.com>
2018-11-08 18:13:23 -0800

    Created Seperate Folder for WebScraping

--- WebScraping/WebScraping-Beautifulsoup.py
+++ WebScraping/WebScraping-Beautifulsoup.py
@@ -10,10 +10,11 @@ html_content = request.text
 
 soup = BeautifulSoup(html_content, "html.parser")
 
+#Find the tag and its attribute
 print(soup.findAll("div", class_ = "paragraph"))
 
 
-
+#Use this model to find a particular element, this example is not restricted to links
 links = (soup.find_all("a"))
 for link in links:
     print(link.get("href"))
